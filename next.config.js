8383117fb0e80ef052c8c95cfc689358f581a325
Luu Hung <73063097+kendyluu810@users.noreply.github.com>
2023-12-30 09:34:18 +0700

    update config

--- next.config.js
+++ next.config.js
@@ -1,15 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+
   experimental: {
     serverComponentsExternalPackages: ["mongoose"],
-    serverRuntimeConfig: {
-      api: {
-        bodyParser: {
-          sizeLimit: "10MB", // Adjust the size limit as needed
-        },
+  },
+
+  serverRuntimeConfig: {
+    api: {
+      bodyParser: {
+        sizeLimit: "10MB", // Adjust the size limit as needed
       },
     },
   },
+
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
