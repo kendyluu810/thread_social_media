aff44091f78d824f749cf845522d5a39380cf6ab
Luu Hung <73063097+kendyluu810@users.noreply.github.com>
2023-12-30 16:06:11 +0700

    update layout sign in

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -25,7 +25,11 @@ export default function RootLayout({
       }}
     >
       <html lang="en">
-        <body className={`${inter.className} bg-dark-1`}>{children}</body>
+        <body className={`${inter.className} bg-dark-1`}>
+          <div className="flex justify-center items-center mt-10 gap-10">
+            {children}
+          </div>
+        </body>
       </html>
     </ClerkProvider>
   );
